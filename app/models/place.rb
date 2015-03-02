c6995cb98f347d6ce8ea9a1cdf11fe7bae8f8cf1
tol3 <tol3@me.com>
2015-03-03 01:28:11 +0700

    add comment model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,8 @@ class Place < ActiveRecord::Base
 
 	# attr_accessible :country_id, :name, :detail, :souvenir
 	belongs_to :country
-
+	has_many :comments, as: :commentable
+	
 	structure do
 		country_id :integer
 		name     :string
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220085933) do
+ActiveRecord::Schema.define(version: 20150302174644) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -45,5 +45,52 @@ ActiveRecord::Schema.define(version: 20150220085933) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content",          limit: 65535
+    t.integer  "user_id",          limit: 4
+    t.integer  "commentable_id",   limit: 4
+    t.string   "commentable_type", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.string   "image",       limit: 255
+    t.datetime "updated_at"
+    t.datetime "created_at"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.integer  "country_id", limit: 4
+    t.string   "name",       limit: 255
+    t.text     "detail",     limit: 65535
+    t.datetime "updated_at"
+    t.datetime "created_at"
+  end
+
+  add_index "places", ["country_id"], name: "index_places_on_country_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
